d534c3853cfe36b7b01dcea9d75ce0d3b29ecd3c
Sealatis <manumanu_64@hotmail.com>
2015-04-21 19:02:16 -0300

    added pieceinLineOfSightbline
    Checks for line of sight using Bresenham algorithm, considers that if a
    piece if next to you you can see it even if its in a no view terrain.

--- src/TacticsPiece.js
+++ src/TacticsPiece.js
@@ -9,14 +9,16 @@ var TacticsPiece  = exports.TacticsPiece= declare({
 	   attackRange 0-3
 	   movementSpeed 0-5
 	   position xxyy 
+	   view distance
 	*/
-	constructor: function TacticsPiece(hp,hitChance,saveChance,attackRange, movement,position){
+	constructor: function TacticsPiece(hp,hitChance,saveChance,attackRange, movement,position,viewDistance){
 		this.position=position;
 		this.hp=hp;
 		this.hitChance=hitChance;
 		this.saveChance=saveChance;
 		this.attackRange=attackRange;
 		this.movement=movement;
+		this.viewDistance=viewDistance;
 
 	},
 
@@ -26,6 +28,41 @@ var TacticsPiece  = exports.TacticsPiece= declare({
 		
 	},
 
+
+
+/** NO TESTING MADE 
+	If a piece if next to me its visible and you should consider terrain. 
+	Calculates if a piece is in line of sight, considering the terrain and type of terrain. 
+	variable:
+	noViewTerrains(  
+	a string representing all the terrains that cut view example x:mountain, y:fog then 
+	noviewTerrains= 'xy')
+	modified version of Bresenham algorithm 
+	http://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#JavaScript
+*/
+	pieceinLineOfSightbline: function pieceinLineOfSightbline(game,piece,noViewTerrains){
+		//FIXME
+ 		x0= this.position[0];
+ 		y0= this.position[1];
+ 		x1= piece.position[0];
+ 		y1= piece.position[1];
+ 		dx = Math.abs(x1 - x0);
+ 		sx = x0 < x1 ? 1 : -1;
+  		dy = Math.abs(y1 - y0);
+  		if (dx <=1 && dy<=1 ){
+  			return true;
+  		}
+  		sy = y0 < y1 ? 1 : -1; 
+  		err = (dx>dy ? dx : -dy)/2;
+		  while (true) {
+		  	if (noViewTerrains.search(game.terrain.square([x0,y0])==-1)){return false;}
+		    if (x0 === x1 && y0 === y1) break;
+		     e2 = err;
+		    if (e2 > -dx) { err -= dy; x0 += sx; }
+		    if (e2 < dy) { err += dx; y0 += sy; }
+		  }
+		return true;  
+	},
 /** Calculates if a piece is in line of sight, considering the terrain and type of terrain. 
 */
 	pieceInLineOfSight: function pieceInLineOfSight(game,piece){
